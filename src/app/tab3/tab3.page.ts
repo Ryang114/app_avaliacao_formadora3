f46d5581bfb1a759f3cbafb8a246a48a964d5d94
ryang114 <rgomespec18@gmail.com>
2026-05-22 00:08:35 -0300

    tab3 finalizada com toda a tabela de produtos

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FakeStoreService } from '../services/fake-store.service';
+import { 
+  IonContent, 
+  IonCard, 
+  IonCardContent, 
+  IonButton,
+  IonSpinner
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonContent, 
+    IonCard, 
+    IonCardContent, 
+    IonButton,
+    IonSpinner
+  ]
 })
-export class Tab3Page {
-  constructor() {}
+export class Tab3Page implements OnInit {
+  products: any[] = [];
+  showList: boolean = false; // Controla se a lista está visível ou oculta
+
+  constructor(private fakeStoreService: FakeStoreService) {}
+
+  ngOnInit() {
+    this.fakeStoreService.getProducts().subscribe({
+      next: (data: any[]) => {
+        this.products = data;
+      },
+      error: (err: any) => {
+        console.error('Erro ao carregar todos os produtos na Tab 3:', err);
+      }
+    });
+  }
+
+  exibirLista() {
+    this.showList = true;
+  }
+
+  ocultarLista() {
+    this.showList = false;
+  }
 }
