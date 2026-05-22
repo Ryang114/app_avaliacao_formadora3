b58c576be080ee38691933a8d378d17daec3750c
ryang114 <rgomespec18@gmail.com>
2026-05-21 23:59:03 -0300

    tab 2 finallizada e api integrada já fucionando

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,55 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FakeStoreService } from '../services/fake-store.service';
+import { 
+  IonContent, 
+  IonCard, 
+  IonCardContent, 
+  IonButton,
+  IonSpinner
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonContent, 
+    IonCard, 
+    IonCardContent, 
+    IonButton,
+    IonSpinner
+  ]
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  products: any[] = [];
+  currentIndex: number = 0; // Controla qual produto está na tela
 
-  constructor() {}
+  constructor(private fakeStoreService: FakeStoreService) {}
 
+  ngOnInit() {
+    this.fakeStoreService.getProducts().subscribe({
+      next: (data: any[]) => {
+        this.products = data; 
+        console.log('PRODUTOS DA API:', data);
+      },
+      error: (err: any) => {
+        console.error('Erro ao buscar produtos:', err);
+      }
+    });
+  }
+
+  nextProduct() {
+    if (this.currentIndex < this.products.length - 1) {
+      this.currentIndex++;
+    }
+  }
+
+  prevProduct() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
 }
